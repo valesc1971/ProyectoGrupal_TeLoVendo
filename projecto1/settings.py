c8facd40bd00cd58ae88b40384cca0d83646a41f
valeria_sanchez_c@hotmail.com <valeria_sanchez_c@hotmail.com>
2022-05-04 21:11:03 -0400

    actualizacion base datos a postgres

--- projecto1/settings.py
+++ projecto1/settings.py
@@ -78,12 +78,19 @@ WSGI_APPLICATION = 'projecto1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projecto1',
+        'USER': 'postgres',
+        'PASSWORD': 'valesc2240',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
