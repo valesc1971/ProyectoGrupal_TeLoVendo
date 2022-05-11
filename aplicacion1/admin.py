16e1f1dd85734bd321b25fa6281701d4b5ec4192
valeria_sanchez_c@hotmail.com <valeria_sanchez_c@hotmail.com>
2022-05-10 21:12:24 -0400

    actualizacion relacion entre modelos

--- aplicacion1/admin.py
+++ aplicacion1/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from .models import Cliente, Comentario, Pedido, Producto, Proveedor
+from django.urls import reverse
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from .models import Cliente, Comentario, Pedido, Producto, Proveedor, Categoria, Subcategoria
+
 
 # Register your models here.
 
 admin.site.register(Cliente)
 admin.site.register(Pedido)
 admin.site.register(Producto)
-admin.site.register(Proveedor)
 admin.site.register(Comentario)
+admin.site.register(Proveedor)
+admin.site.register(Categoria)
+admin.site.register(Subcategoria)
+
+"""
+class ProveedorAdmin(admin.ModelAdmin):
+
+    list_display = ["categoria", "subcategoria", "related"]
+    list_display_links = ["categoria", "related"]
+
+"""
--- aplicacion1/models.py
+++ aplicacion1/models.py
@@ -25,15 +25,33 @@ class Producto(models.Model):
     cantidad=models.CharField(max_length=50)
     proveedor=models.CharField(max_length=50)
 
+class Subcategoria(models.Model):
+    nombre=models.CharField(max_length=50, verbose_name="nombre subcategoria")
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre=models.CharField(max_length=50, verbose_name="nombre categoria")
+
+    def __str__(self):
+        return self.nombre
+
 class Proveedor(models.Model):
     nombre=models.CharField(max_length=50)
-    categoria=models.CharField(max_length=50)
+    categoria=models.ForeignKey(Categoria, null=True, on_delete=models.CASCADE)
+    subcategoria=models.ForeignKey(Subcategoria, null=True, on_delete=models.CASCADE) 
     correo=models.EmailField(max_length=100)
     ciudad=models.CharField(max_length=50)
     telefono=models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
 
 class Comentario(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     correo=models.EmailField(max_length=100)
     comentario=models.CharField(max_length=150)
+
+
